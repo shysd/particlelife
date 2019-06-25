9b9c3a5de4318416962b12ab59ae28a593b6e9e5
Shreyas Dethe <shreyasdethe9@gmail.com>
2019-06-25 19:14:41 +0530

    Update files

--- particle.js
+++ particle.js
@@ -1,9 +1,9 @@
 class Particle {
-	constructor(type){
-		this.type = type;
+	constructor(vel){
+		this.type = random([1, 2, 3]);
 		this.r = 10;
 		this.pos = createVector(random(width), random(height));
-		this.vel = createVector(1, 0);
+		this.vel = createVector(0.5, 0);
 	}
 
 	show(){
@@ -25,9 +25,27 @@ class Particle {
 		}
 
 		ellipse(this.pos.x, this.pos.y, this.r, this.r);
+
+		if(this.pos.x > width - this.r) this.pos.x = width - this.r;
+		if(this.pos.y > height - this.r) this.pos.y = height - this.r;
+		if(this.pos.x < this.r) this.pos.x = 0 + this.r;
+		if(this.pos.y < this.r) this.pos.y = 0 + this.r;
 	}
 
+
 	update(){
-		this.pos = p5.Vector.add(this.pos, this.vel);
+		// this.pos = this.pos.add(p5.Vector.mult(p5.Vector.fromAngle(degrees(random(0, 360))), 5));
+	}
+
+	attract(neighbour){
+		var velc = p5.Vector.sub(neighbour.pos, this.pos);
+		velc.normalize();
+		this.pos.add(velc);
+	}
+
+	repel(neighbour){
+		var velc = p5.Vector.sub(this.pos, neighbour.pos);
+		velc.normalize();
+		this.pos.add(velc);
 	}
 }
--- sketch.js
+++ sketch.js
@@ -2,15 +2,33 @@ function setup(){
 	createCanvas(1280, 720);
 	particles = new Array(250);
 	for(var i = 0; i < particles.length; i++){
-		particles[i] = new Particle(random([1, 2, 3]));
+		particles[i] = new Particle();
 	}
 }
 
 function draw(){
+	// frameRate(1);
 	background(50);
 	stroke(50);
 	for(var i = 0; i < particles.length; i++){
 		particles[i].show();
-		particles[i].update();
 	}
+
+
+	var criticaldist = 50;
+	for(var i = 0; i < particles.length; i++){
+		for(var j = 0; j < particles.length; j++){
+			if(i != j){
+				let dist = particles[i].pos.dist(particles[j].pos);
+				if(particles[i].type == particles[j].type && dist < criticaldist){
+					particles[i].attract(particles[j]);
+				}
+				else if(particles[i].type != particles[j].type && dist < criticaldist){
+					particles[i].repel(particles[j]);
+				}
+			}
+		}
+	}
+
+	// noLoop();
 }
